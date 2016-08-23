f6b4f7cfe8afa9d93ff140c8c5b830f61b35785f
ptlergo <ptlergo@gmail.com>
2016-08-22 23:18:13 -0400

    dublicated exports

--- src/server.js
+++ src/server.js
@@ -16,3 +16,51 @@ exports.debug = (msg, status) => {
     }
   }
 };
+
+exports.log = (msg, status) => {
+  const seperator = '\n=============================\n';
+  if (process.env.DEBUG) {
+    const coloredmsg = color.green(msg);
+    const warnmsg = color.yellow(msg);
+    const errormsg = color.red(msg);
+    if (status === undefined) {
+      console.log(coloredmsg + seperator);
+    } else if (status === 444) {
+      console.log(errormsg + seperator);
+    } else {
+      console.log(coloredmsg, status + seperator);
+    }
+  }
+};
+
+exports.warn = (msg, status) => {
+  const seperator = '\n=============================\n';
+  if (process.env.DEBUG) {
+    const coloredmsg = color.green(msg);
+    const warnmsg = color.yellow(msg);
+    const errormsg = color.red(msg);
+    if (status === undefined) {
+      console.log(coloredmsg + seperator);
+    } else if (status === 444) {
+      console.log(errormsg + seperator);
+    } else {
+      console.log(coloredmsg, status + seperator);
+    }
+  }
+};
+
+exports.error = (msg, status) => {
+  const seperator = '\n=============================\n';
+  if (process.env.DEBUG) {
+    const coloredmsg = color.green(msg);
+    const warnmsg = color.yellow(msg);
+    const errormsg = color.red(msg);
+    if (status === undefined) {
+      console.log(coloredmsg + seperator);
+    } else if (status === 444) {
+      console.log(errormsg + seperator);
+    } else {
+      console.log(coloredmsg, status + seperator);
+    }
+  }
+};
