cb0405d6601a6ec7696b743901155c1ded6bcd67
ptlergo <ptlergo@gmail.com>
2016-08-22 23:12:22 -0400

    added mocha tests

--- test/__log.js
+++ test/__log.js
@@ -5,10 +5,32 @@ require('mocha-sinon')
 const expect = require('chai').expect;
 
 describe('Console log', () => {
-  it('Should be able to console log', () => {
+  it('Should be able to console log', (done) => {
     let logger = sinon.spy();
     logger(utility.debug('Logging'));
     expect(logger.callCount).to.equal(1);
+    done();
+  });
+
+  it('Should be able to console error', (done) => {
+    let logger = sinon.spy();
+    logger(utility.error('Logging'));
+    expect(logger.callCount).to.equal(1);
+    done();
+  });
+
+  it('Should be able to console warn', (done) => {
+    let logger = sinon.spy();
+    logger(utility.warn('Logging'));
+    expect(logger.callCount).to.equal(1);
+    done();
+  });
+
+  it('Should be able to console log with debug()', (done) => {
+    let logger = sinon.spy();
+    logger(utility.log('Logging'));
+    expect(logger.callCount).to.equal(1);
+    done();
   });
 
   it('Should be able to append to a file', (done) => {
